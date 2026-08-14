a064f0c4481e49c10c83a58a2bfa17d534018b08
dhritibangera-cmd <dhritibangera@gmail.com>
2026-08-14 11:56:38 +0530

    Create sort list.py

---
+++ sort list.py
@@ -0,0 +1,48 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        # Find the middle of the linked list
+        slow = head
+        fast = head.next
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # Split the list into two halves
+        mid = slow.next
+        slow.next = None
+
+        # Sort both halves
+        left = self.sortList(head)
+        right = self.sortList(mid)
+
+        # Merge the sorted halves
+        dummy = ListNode(0)
+        current = dummy
+
+        while left and right:
+            if left.val < right.val:
+                current.next = left
+                left = left.next
+            else:
+                current.next = right
+                right = right.next
+
+            current = current.next
+
+        # Attach remaining nodes
+        if left:
+            current.next = left
+        else:
+            current.next = right
+
+        return dummy.next
